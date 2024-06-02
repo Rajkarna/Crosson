792a0b8fa6842219bc1fb6645e103684e8065cc5
Raj Kumar <rajkumarsa926@gmail.com>
2024-06-02 17:16:22 +0530

    example push

--- Crosson/src/App.jsx
+++ Crosson/src/App.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import { experience } from './typography/typography'
-import './typography/typography.scss';
-import Header from './components/Header/Header';
-import Slider from './components/Slider/slider';
-import WhoAreWe from './components/Who-are-we/WhoAreWe';
-import WhatWeDo from './components/What-we-do/WhatWeDo';
-import './App.scss';
-import Detail from './components/Detail/Detail';
-import HowWeDo from './components/How-we-do/how-we-do';
-import News from './components/News/News';
-
+import React from "react";
+import { experience } from "./typography/typography";
+import "./typography/typography.scss";
+import Header from "./components/Header/Header";
+import Slider from "./components/Slider/slider";
+import WhoAreWe from "./components/Who-are-we/WhoAreWe";
+import WhatWeDo from "./components/What-we-do/WhatWeDo";
+import "./App.scss";
+import Detail from "./components/Detail/Detail";
+import HowWeDo from "./components/How-we-do/how-we-do";
+import News from "./components/News/News";
 
 const App = () => {
   return (
     <>
+      <Header />
+      <div>
+        <Slider />
+      </div>
+      <div>
+        <WhoAreWe />
+      </div>
+      <WhatWeDo />
 
-    <div><Slider/></div>
-    <div><WhoAreWe/></div>
-    <WhatWeDo/>
- 
-
-    <News />
-
+      <Detail />
+    
+      <News />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
